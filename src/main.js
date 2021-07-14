bf8ed3cfc1b0243c0a8a93c2942799781721943a
zulwaran <81923601+zulwaran@users.noreply.github.com>
2021-07-15 00:21:05 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Vuelidate from 'vuelidate'
+
+Vue.config.productionTip = false
+Vue.config.devtools = true
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
+
+Vue.use(Vuelidate)
